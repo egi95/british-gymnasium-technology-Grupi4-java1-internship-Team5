fa52b0d489138c2d33f5300dab78ecab88a8e828
ErisKeq1 <ek186@bgt.school>
2025-07-01 11:10:49 +0200

    Update AfternoonTask1.c

--- Day1/AfternoonTask1.c
+++ Day1/AfternoonTask1.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+int main()
+{
+  //printf("Eris Keqi");
+  
+  //Detyar e 2
+  //printf("Emri:\tEris\n");
+  //printf("Mosha:\t17\n");
+  //printf("Qyteti:\tPrshtina\n");
+  
+  //Detra e 3
+  #include <stdio.h>     // Përfshin librarinë standarde për input/output
 
+
+int main() {
+    // Shfaq tekstin "Emri:" dhe "Arben", me një hapësirë tab ndërmjet
+    printf("Emri:\tArben\n");
+
+
+    // Shfaq tekstin "Mosha:" dhe "20", me një hapësirë tab ndërmjet
+    printf("Mosha:\t20\n");
+
+
+    // Shfaq tekstin "Qyteti:" dhe "Prishtine", me një hapësirë tab ndërmjet
+    printf("Qyteti:\tPrishtine\n");
+
+
+    return 0;   // Kthen 0 për të treguar që programi përfundoi me sukses
+    
+    //Detyra e 4 
+    include <stdio.h>     // Përfshin librarinë standarde për input/output
+
+
+int main() {
+    // Shfaq tekstin "Emri:" dhe "Arben", me një hapësirë tab ndërmjet
+    printf("Emri:\tArben\n");
+
+
+    // Shfaq tekstin "Mosha:" dhe "20", me një hapësirë tab ndërmjet
+    printf("Mosha:\t20\n");
+
+
+    // Shfaq tekstin "Qyteti:" dhe "Prishtine", me një hapësirë tab ndërmjet
+    printf("Qyteti:\tPrishtine\n");
+
+
+    return 0;   // Kthen 0 për të treguar që programi përfundoi me sukses
+}
+  
+
+
+   
+  
+  
+  
+  
+}

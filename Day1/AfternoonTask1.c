4f71a144a8ee60d943c3996e244b9ade9c4f3fa5
Jora169 <jg160@bgt.school>
2025-07-01 11:11:26 +0200

    Update AfternoonTask1.c

--- Day1/AfternoonTask1.c
+++ Day1/AfternoonTask1.c
@@ -1,1 +1,12 @@
+#include <stdio.h>  // Biblioteka standarde për input/output
 
+int main() {
+    // Shfaq titullin e programit dhe kalon në rreshtin tjetër
+    printf("Shfaqja e emrit tim:\n");
+
+    // Shton një hapësirë me tab dhe shfaq emrin
+    printf("\tJora Gashi\n");
+
+    // Kthen vlerën 0 për të treguar që programi përfundoi me sukses
+    return 0;
+}

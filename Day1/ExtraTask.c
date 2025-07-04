10bc42c62764f5ba25d2ed0c00426d70541210d9
Jora169 <jg160@bgt.school>
2025-07-04 14:10:24 +0200

    Update ExtraTask.c

--- Day1/ExtraTask.c
+++ Day1/ExtraTask.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+    // Kjo shtyp vijën e sipërme të kutisë
+    printf("*********************\n");
+
+    // Rresht me hapësirë boshe
+    printf("*                   *\n");
+
+    // Shtyp inicialet brenda kutisë duke përdorur \t për hapësirë
+    // \t është për tab (hapësirë horizontale)
+    printf("*     J. G.         *\n");
+
+    // Rresht me hapësirë boshe
+    printf("*                   *\n");
+
+    // Kjo shtyp vijën e poshtme të kutisë
+    printf("*********************\n");
+
+    return 0;
+}

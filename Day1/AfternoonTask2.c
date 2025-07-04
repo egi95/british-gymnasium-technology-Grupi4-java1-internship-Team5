76d68166fc2f7ec05fafdec29f3cd03f03260626
Jora169 <jg160@bgt.school>
2025-07-04 14:09:24 +0200

    Update AfternoonTask2.c

--- Day1/AfternoonTask2.c
+++ Day1/AfternoonTask2.c
@@ -1,1 +1,9 @@
-
+git init
+echo "Emri: Jora Gashi\nData: 2 Korrik 2025" > README.md
+touch program.c
+git add README.md program.c
+git commit -m "Initial commit"
+git switch -c development
+# (ndrysho program.c)
+git add program.c
+git commit -m "Shtuar mesazh për degën development"
